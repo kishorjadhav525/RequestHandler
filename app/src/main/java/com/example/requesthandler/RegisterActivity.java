d7621946b0457a47ceaa345f54954e6cd9e766a5
akshaybhandari18 <akshaybhandari394@gmail.com>
2020-01-18 14:56:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/requesthandler/RegisterActivity.java
@@ -0,0 +1,101 @@
+package com.example.requesthandler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+public class RegisterActivity extends AppCompatActivity {
+    EditText ed_username, ed_email, ed_password;
+    public static final String URL_REGISTER = "http://192.168.43.17/login-register/register.php";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        ed_username = findViewById(R.id.ed_username);
+        ed_email = findViewById(R.id.ed_email);
+        ed_password = findViewById(R.id.ed_password);
+
+    }
+
+    public void register(View view){
+        final String username = ed_username.getText().toString();
+        final String email = ed_email.getText().toString();
+        final String password = ed_password.getText().toString();
+
+        if(username.isEmpty() || email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
+        }
+
+        else {
+            class Login extends AsyncTask<Void, Void, String> {
+                ProgressDialog pdLoading = new ProgressDialog(RegisterActivity.this);
+
+                @Override
+                protected void onPreExecute() {
+                    super.onPreExecute();
+
+                    //this method will be running on UI thread
+                    pdLoading.setMessage("\tLoading...");
+                    pdLoading.setCancelable(false);
+                    pdLoading.show();
+                }
+
+                @Override
+                protected String doInBackground(Void... voids) {
+                    //creating request handler object
+                    RequestHandler requestHandler = new RequestHandler();
+
+                    //creating request parameters
+                    HashMap<String, String> params = new HashMap<>();
+                    params.put("email", email);
+                    params.put("password", password);
+                    params.put("username", username);
+
+                    //returing the response
+                    return requestHandler.sendPostRequest(URL_REGISTER, params);
+                }
+
+                @Override
+                protected void onPostExecute(String s) {
+                    super.onPostExecute(s);
+                    pdLoading.dismiss();
+
+                    try {
+                        //converting response to json object
+                        JSONObject obj = new JSONObject(s);
+                        //if no error in response
+                        if (!obj.getBoolean("error")) {
+                            Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_LONG).show();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(RegisterActivity.this, "Exception: " + e, Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+
+            Login login = new Login();
+            login.execute();
+        }
+    }
+
+    public void login(View view){
+        finish();
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
+
